b6b8e9bfed697c5015d9204c049fdf6f69fba2b3
Luke Clifford <luke@firesky.com.au>
2015-11-19 00:07:27 +1100

    Add post for todos

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Shoot the barman',
-    complete: false
-}, {
-    id: 2,
-    description: 'Go to sleep',
-    complete: false
-},{
-    id: 3,
-    description: 'Wha you wan',
-    complete: true
-}];
+var todos = [];
+var todoNextId = 1;
 
-
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
     res.send('Todo API root')
@@ -44,6 +34,16 @@ app.get('/todos/:id', function (req, res){
         res.status(404).send();
     }
     
+});
+
+app.post('/todos', function (req,res) {
+    var body = req.body;
+    
+    body.id = todoNextId++;
+    todos.push(body);
+    
+    //console.log('description: '+body.description );
+    res.json(body);
 });
 
 app.listen(PORT, function () {
