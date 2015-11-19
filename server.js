67b3495eeab88d3585f0e106f65c2c29e61762b6
Luke Clifford <luke@firesky.com.au>
2015-11-19 18:28:11 +1100

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
+
+
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
@@ -22,23 +26,23 @@ app.get('/todos', function (req, res) {
 app.get('/todos/:id', function (req, res){
     //res.send('Asking for todo with id of ' +req.params.id);
     var todoid = parseInt(req.params.id,10);
-    var matchedtodo;
-    for (var i=0; i<todos.length; i++){
-        if(todos[i].id === todoid){
-            matchedtodo = todos[i];
-        }
-    }
+    
+    var matchedtodo = _.findWhere(todos, {id:todoid})
+
     if(matchedtodo){
         res.json(matchedtodo);  
     }else{
         res.status(404).send();
     }
-    
 });
 
 app.post('/todos', function (req,res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description','completed');
     
+    if(!_.isBoolean(!body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+        return res.status(400).send();
+    }
+    body.description = body.description.trim();
     body.id = todoNextId++;
     todos.push(body);
     
