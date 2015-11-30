3cb7ddb7d7939b0a4a188cd7d32d2213ce4e7e04
Luke Clifford <luke@firesky.com.au>
2015-11-30 23:58:48 +1100

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,9 @@
-module.exports = function(sequelize, DataTypes) {
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
+
+
+module.exports = function (sequelize, DataTypes) {
     return sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
@@ -8,19 +13,39 @@ module.exports = function(sequelize, DataTypes) {
                 isEmail: true
             }
         },
-        password: {
+        salt: {
             type: DataTypes.STRING,
+        },
+        password_hash: {
+            type: DataTypes.STRING,
+        },
+        password: {
+            type: DataTypes.VIRTUAL,
             allowNull: false,
-            validate:{
-                len: [7,100]
+            validate: {
+                len: [7, 100]
+            },
+            set: function (value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
     }, {
         hooks: {
-            beforeValidate: function(user, otions){
-                if(typeof user.email === 'string'){
+            beforeValidate: function (user, otions) {
+                if (typeof user.email === 'string') {
                     user.email = user.email.toLowerCase();
                 }
+            }
+        },
+        instanceMethods: {
+            topublicJson: function(){
+                var json = this.toJSON();
+                return _.pick(json, 'id','email','createdAt', 'updatedAt');
             }
         }
     });
